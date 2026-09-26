e752a3a35e627f75d466ca270fd7e395585cbd4a
Yash Makwana <yash.009675@gmail.com>
2026-09-26 13:18:29 +0530

    feat: add Firebase messaging service worker

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,13 @@
+importScripts("https://www.gstatic.com/firebasejs/10.13.2/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.13.2/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDI4HUTnnyxBqiBYNRiS9zadEG15yqTZQU",
+  authDomain: "apna-store-949a7.firebaseapp.com",
+  projectId: "apna-store-949a7",
+  storageBucket: "apna-store-949a7.firebasestorage.app",
+  messagingSenderId: "923907531359",
+  appId: "1:923907531359:web:e8a415af52536b9c10f1ea"
+});
+
+firebase.messaging();
